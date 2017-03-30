c1c9f725b5582a9defabe8e4d8ab531d5f547902
Simon Olsson <simon.olsson@gmail.com>
2017-03-30 17:11:38 +0200

    Fonts and stuff

--- packages/fyndiq-ui-test/stories/story-productcard.js
+++ packages/fyndiq-ui-test/stories/story-productcard.js
@@ -5,7 +5,7 @@ import Productcard from 'fyndiq-component-productcard'
 storiesOf('Product card', module)
 
   .addDecorator((story) => (
-    <div style={{backgroundColor: 'pink', padding: '1em'}}>
+    <div style={{backgroundColor: '#c4e5e9', padding: '1em'}}>
       {story()}
     </div>
   ))
@@ -14,6 +14,6 @@ storiesOf('Product card', module)
     </Productcard>
   ))
   .addWithInfo('Extra card', () => (
-    <Productcard title="Vita Konstläder Sneakers - Ombloggade Street skor" price="149 kr" oldprice="499 kr" url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png">
+    <Productcard title="Wheeler Taklampa Koppar" price="599 kr" oldprice="1390 kr" url="https://cdn.fyndiq.se/product/e8/74/3c/a1a451ba9af03f19e6bdd54890bd05b6ec/original.png">
     </Productcard>
   ))
