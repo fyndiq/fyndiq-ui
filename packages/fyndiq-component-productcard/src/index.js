5d869cb98d96176a320172533e825b9ec788bb5c
Simon Olsson <simon.olsson@gmail.com>
2017-03-30 17:35:14 +0200

    Stars on product card

--- packages/fyndiq-component-productcard/src/index.js
+++ packages/fyndiq-component-productcard/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Price, CurrentPrice, OldPrice } from 'fyndiq-component-price'
+import { Stars } from 'fyndiq-component-stars'
 import styles from '../styles.less'
 
-const Productcard = ({ price, title, url, oldprice }) => (
+const Productcard = ({ price, title, url, oldprice, rating }) => (
   <div className={ styles.card }>
     <img className={ styles.image} src={ url }/>
     <Price>
@@ -10,6 +11,8 @@ const Productcard = ({ price, title, url, oldprice }) => (
       <CurrentPrice>{ price }</CurrentPrice>
     </Price>
     <h3 className={ styles.title }>{ title }</h3>
+
+    <Stars rating={ rating }></Stars>
   </div>
 )
 
--- packages/fyndiq-ui-test/stories/story-productcard.js
+++ packages/fyndiq-ui-test/stories/story-productcard.js
@@ -10,10 +10,15 @@ storiesOf('Product card', module)
     </div>
   ))
   .addWithInfo('Regular card', () => (
-    <Productcard title="Vita Konstläder Sneakers - Ombloggade Street skor" price="149 kr" url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png">
+    <Productcard title="Vita Konstläder Sneakers - Ombloggade Street skor" price="149 kr" url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png" rating={ 4 }>
     </Productcard>
   ))
   .addWithInfo('With old price', () => (
-    <Productcard title="Wheeler Taklampa Koppar" price="599 kr" oldprice="1390 kr" url="https://cdn.fyndiq.se/product/e8/74/3c/a1a451ba9af03f19e6bdd54890bd05b6ec/original.png">
-    </Productcard>
+    <Productcard 
+      title="Wheeler Taklampa Koppar" 
+      price="599 kr" 
+      oldprice="1390 kr" 
+      url="https://cdn.fyndiq.se/product/e8/74/3c/a1a451ba9af03f19e6bdd54890bd05b6ec/original.png"
+      rating={ 4 }
+    />
   ))
