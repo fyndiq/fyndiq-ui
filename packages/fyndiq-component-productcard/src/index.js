644d8461f72653e613f5cc962e1448682b47d3e8
Simon Olsson <simon.olsson@gmail.com>
2017-03-30 16:52:41 +0200

    Nice product cards

--- packages/fyndiq-component-productcard/src/index.js
+++ packages/fyndiq-component-productcard/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Price, CurrentPrice } from 'fyndiq-component-price'
+import { Price, CurrentPrice, OldPrice } from 'fyndiq-component-price'
 import styles from '../styles.less'
 
-const Productcard = ({ price, title, url }) => (
-  <div>
-    <img src={ url }/>
+const Productcard = ({ price, title, url, oldprice }) => (
+  <div className={ styles.card }>
+    <img className={ styles.image} src={ url }/>
     <Price>
+      <OldPrice>{ oldprice }</OldPrice>
       <CurrentPrice>{ price }</CurrentPrice>
     </Price>
-    <h3>{ title }</h3>
+    <h3 className={ styles.title }>{ title }</h3>
   </div>
 )
 
--- packages/fyndiq-ui-test/stories/story-productcard.js
+++ packages/fyndiq-ui-test/stories/story-productcard.js
@@ -3,7 +3,11 @@ import { storiesOf, action, linkTo } from '@kadira/storybook'
 import Productcard from 'fyndiq-component-productcard'
 
 storiesOf('Product card', module)
-  .addWithInfo('Productcard', () => (
+  .addWithInfo('Regular card', () => (
+    <Productcard title="Vita Konstläder Sneakers - Ombloggade Street skor" price="149 kr" url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png">
+    </Productcard>
+  ))
+  .addWithInfo('Extra card', () => (
     <Productcard title="Vita Konstläder Sneakers - Ombloggade Street skor" price="149 kr" oldprice="499 kr" url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png">
     </Productcard>
   ))
