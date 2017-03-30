7326b1180fa4d5bfa674138ba17325b0b42df1eb
Simon Olsson <simon.olsson@gmail.com>
2017-03-30 17:23:32 +0200

    Rename he he

--- packages/fyndiq-ui-test/stories/story-productcard.js
+++ packages/fyndiq-ui-test/stories/story-productcard.js
@@ -13,7 +13,7 @@ storiesOf('Product card', module)
     <Productcard title="Vita Konstläder Sneakers - Ombloggade Street skor" price="149 kr" url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png">
     </Productcard>
   ))
-  .addWithInfo('Extra card', () => (
+  .addWithInfo('With old price', () => (
     <Productcard title="Wheeler Taklampa Koppar" price="599 kr" oldprice="1390 kr" url="https://cdn.fyndiq.se/product/e8/74/3c/a1a451ba9af03f19e6bdd54890bd05b6ec/original.png">
     </Productcard>
   ))
