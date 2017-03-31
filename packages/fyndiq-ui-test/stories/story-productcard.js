89370bb71019a04242d1fd7596215dd3ad6384c6
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-03-31 15:18:44 +0200

    Remove trailing spaces

--- packages/fyndiq-ui-test/stories/story-productcard.js
+++ packages/fyndiq-ui-test/stories/story-productcard.js
@@ -10,26 +10,26 @@ storiesOf('Product card', module)
     </div>
   ))
   .addWithInfo('Regular card', () => (
-    <Productcard 
-      title="Vita Konstläder Sneakers - Ombloggade Street skor" 
-      price="149 kr" 
-      url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png" 
+    <Productcard
+      title="Vita Konstläder Sneakers - Ombloggade Street skor"
+      price="149 kr"
+      url="https://cdn.fyndiq.se/product/d1/de/26/2a08917d54bf6726e70441b86d86400d9e/original.png"
     />
   ))
   .addWithInfo('With old price', () => (
-    <Productcard 
-      title="Wheeler Taklampa Koppar" 
-      price="599 kr" 
-      oldprice="1390 kr" 
+    <Productcard
+      title="Wheeler Taklampa Koppar"
+      price="599 kr"
+      oldprice="1390 kr"
       url="https://cdn.fyndiq.se/product/e8/74/3c/a1a451ba9af03f19e6bdd54890bd05b6ec/original.png"
     />
   ))
  .addWithInfo('With rating', () => (
-    <Productcard 
+    <Productcard
       title="Spirelli-Spagettiskärare för rawfood"
-      price="199 kr" 
-      oldprice="399 kr" 
+      price="199 kr"
+      oldprice="399 kr"
       url= "https://cdn.fyndiq.se/product/f7/e7/ea/15f91dc30f5b264568c551a9842863d0c9/original.png"
-      rating={ 4 }
+      rating={4.4}
     />
   ))
