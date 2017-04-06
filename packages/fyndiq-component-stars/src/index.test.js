ef3908c013f11be4a59554a2c834618be8d5e044
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-04-06 17:39:29 +0200

    Add enzyme-to-json JEST serializer
    This enables snapshot matching for enzyme components

--- packages/fyndiq-component-stars/src/index.test.js
+++ packages/fyndiq-component-stars/src/index.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
 import Stars from './'
@@ -31,5 +32,22 @@ describe('fyndiq-component-stars', () => {
   it('should be rendered with prop reviews', () => {
     const component = renderer.create(<Stars interactive />)
     expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('should send an event if interactive and clicked on', () => {
+    const clickSpy = jest.fn()
+    const component = shallow(<Stars interactive onChange={clickSpy} />)
+
+    // Find the 3rd star (at is 0-index based)
+    component.find('Star').at(2).simulate('click')
+
+    expect(clickSpy.mock.calls).toHaveLength(1)
+    expect(clickSpy.mock.calls[0][0]).toBe(3)
+  })
+
+  it('should change its state when hovered and interactive', () => {
+    const component = shallow(<Stars interactive />)
+    component.find('Star').at(2).simulate('hover')
+    expect(component).toMatchSnapshot()
   })
 })
