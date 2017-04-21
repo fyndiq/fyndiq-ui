adab3cfd410cd190d68e311351f083e5147acfca
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-04-21 15:12:04 +0200

    Update snapshots and tests for new component

--- packages/fyndiq-component-dropdown/src/index.test.js
+++ packages/fyndiq-component-dropdown/src/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import Dropdown from './'
 
@@ -18,19 +18,19 @@ describe('fyndiq-component-dropdown', () => {
 
   test('should have a custom wrapper', () => {
     expect(shallow(
-      <Dropdown button="B" noWrapperStyle>Content</Dropdown>
+      <Dropdown button="B" noWrapperStyle opened>Content</Dropdown>
     ).find('.dropdownWrapper')).toMatchSnapshot()
   })
 
   test('should be opened when clicked on the button', () => {
-    const component = shallow(<Dropdown button="button">Value</Dropdown>)
+    const component = mount(<Dropdown button="button">Value</Dropdown>)
     component.find('div > div').at(0).simulate('click')
     expect(component.find('.dropdownWrapper')).toMatchSnapshot()
   })
 
   test('should be opened when hovered, if in hoverMode', () => {
     jest.useFakeTimers()
-    const component = shallow(<Dropdown button="button" hoverMode>Value</Dropdown>)
+    const component = mount(<Dropdown button="button" hoverMode>Value</Dropdown>)
     component.simulate('mouseover')
     jest.runTimersToTime(200)
     expect(component.find('.dropdownWrapper')).toMatchSnapshot()
@@ -41,13 +41,13 @@ describe('fyndiq-component-dropdown', () => {
 
   test('should have different dropdown positions', () => {
     expect(shallow(
-      <Dropdown button="B" position="bc">Content</Dropdown>
+      <Dropdown button="B" position="bc" opened>Content</Dropdown>
     ).find('.dropdownWrapper')).toMatchSnapshot()
   })
 
   test('should have an arrow oriented to the top if dropdown is above', () => {
     expect(shallow(
-      <Dropdown button="B" position="tr">Content</Dropdown>
+      <Dropdown button="B" position="tr" opened>Content</Dropdown>
     ).find('Arrow')).toMatchSnapshot()
   })
 })
