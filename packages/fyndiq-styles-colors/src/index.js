352e507feda7014f7cd3fa064a31506a2426bf51
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-10-16 13:40:28 +0200

    :eyes: Update colors
    - black
    - lightgrey
    - red
    - blue
    - border

--- packages/fyndiq-styles-colors/src/index.js
+++ packages/fyndiq-styles-colors/src/index.js
@@ -1,18 +1,23 @@
 export default {
-  red: '#EC514A',
+  /* Brand color */
   brand: '#EC514A',
 
-  black: '#333',
+  /* Grey levels */
+  black: '#3B4257',
   darkgrey: '#666',
-  lightgrey: '#999',
+  lightgrey: '#7E7E7E',
   lightergrey: '#BBB',
   palegrey: '#F2F2F2',
   white: '#FFF',
 
-  blue: '#8AB7E3',
+  /* Theme colors */
+  red: '#E74A51',
+  blue: '#4A90E2',
   green: '#70B65A',
   yellow: '#FFD600',
+  'yellow-deal': '#FE0',
   orange: '#F08C48',
 
-  bordercolor: '#DADADA',
+  /* Component colors */
+  border: '#CCD1DD',
 }
