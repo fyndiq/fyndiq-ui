239d8995b89fccdaef86ed61b4d4bc9fd1c47421
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-10-30 13:07:09 +0100

    :new: Add ModalButton Component

---
+++ packages/fyndiq-component-modal/src/modal-button.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from 'fyndiq-component-button'
+
+import Modal from './modal'
+
+// Proptype checker: checks that the element is of a specific Component type
+const ElementOfType = type => (props, propName, componentName) => {
+  const prop = props[propName]
+  if (React.isValidElement(prop) && prop.type === type) return null
+
+  // Otherwise, return Error
+  return new Error(
+    `Invalid prop \`${propName}\` supplied to` +
+      ` \`${componentName}\`. Must be of type ${type.displayName || type.name}`,
+  )
+}
+
+class ModalButton extends React.Component {
+  static propTypes = {
+    button: PropTypes.oneOfType([PropTypes.string, ElementOfType(Button)]),
+    children: PropTypes.oneOfType([PropTypes.node, ElementOfType(Modal)]),
+  }
+
+  static defaultProps = {
+    button: 'Open Modal',
+    children: null,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { open: false }
+
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+  }
+
+  getButton() {
+    const { button } = this.props
+    if (React.isValidElement(button) && button.type === Button) {
+      return React.cloneElement(button, {
+        onClick: this.openModal,
+      })
+    }
+
+    return <Button onClick={this.openModal}>{button}</Button>
+  }
+
+  getModal() {
+    const { children } = this.props
+
+    const modalProps = {
+      open: this.state.open,
+      onClose: this.closeModal,
+    }
+    if (React.isValidElement(children) && children.type === Modal) {
+      return React.cloneElement(this.props.children, modalProps)
+    }
+
+    return <Modal {...modalProps}>{children}</Modal>
+  }
+
+  closeModal() {
+    this.setState({ open: false })
+  }
+
+  openModal() {
+    this.setState({ open: true })
+  }
+
+  render() {
+    return (
+      <span>
+        {this.getButton()}
+        {this.getModal()}
+      </span>
+    )
+  }
+}
+
+export default ModalButton
--- packages/fyndiq-ui-test/stories/component-modal.js
+++ packages/fyndiq-ui-test/stories/component-modal.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import Modal from 'fyndiq-component-modal'
+import Button from 'fyndiq-component-button'
+import Modal, { ModalButton } from 'fyndiq-component-modal'
 
-storiesOf('Modal', module).addWithInfo('default', () => (
-  <Modal open>Hello</Modal>
-))
+import './component-modal.css'
+
+storiesOf('Modal', module)
+  .addWithInfo('default', () => (
+    <ModalButton button="Open Modal">Content</ModalButton>
+  ))
+  .addWithInfo('custom modal styles', () => (
+    <ModalButton button="Open Modal">
+      <Modal
+        overlayClassName="test-overlay--red"
+        wrapperClassName="test-wrapper--black"
+      >
+        Content with black background on a red transparent overlay
+      </Modal>
+    </ModalButton>
+  ))
+  .addWithInfo('custom ModalButton', () => (
+    <ModalButton button={<Button type="primary">Open Modal</Button>}>
+      Content
+    </ModalButton>
+  ))
