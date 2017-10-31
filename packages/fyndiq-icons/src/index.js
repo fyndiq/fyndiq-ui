f765ed6b019007ce8b450e3e2a51dd8457010cdb
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-10-31 15:31:58 +0100

    :new: Add pencil icon

--- packages/fyndiq-icons/src/index.js
+++ packages/fyndiq-icons/src/index.js
@@ -2,5 +2,6 @@ import Arrow from './arrow'
 import Star from './star'
 import Checkmark from './checkmark'
 import Magnifier from './magnifier'
+import Pencil from './pencil'
 
-export { Arrow, Star, Checkmark, Magnifier }
+export { Arrow, Star, Checkmark, Magnifier, Pencil }
