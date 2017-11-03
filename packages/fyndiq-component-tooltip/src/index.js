88264b7bf077225114c4ecb554c8ea7b2199bef1
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-11-03 11:55:32 +0100

    :wrench: Tooltip is now a React Component

--- packages/fyndiq-component-tooltip/src/index.js
+++ packages/fyndiq-component-tooltip/src/index.js
@@ -4,37 +4,46 @@ import Dropdown from 'fyndiq-component-dropdown'
 
 import styles from '../styles.css'
 
-const Tooltip = ({ text, children, position, className }) => (
-  <Dropdown
-    button={<span>{children}</span>}
-    hoverMode
-    noWrapperStyle
-    position={position}
-  >
-    <div
-      className={`
-        ${styles.tooltip}
-        ${styles[`position-${position}`]}
-        ${className}
-      `}
-    >
-      {text}
-    </div>
-  </Dropdown>
-)
+class Tooltip extends React.Component {
+  static propTypes = {
+    text: PropTypes.node,
+    className: PropTypes.string,
+    children: PropTypes.node,
+    position: Dropdown.propTypes.position, // eslint-disable-line react/no-typos
+  }
 
-Tooltip.propTypes = {
-  text: PropTypes.node,
-  className: PropTypes.string,
-  children: PropTypes.node,
-  position: Dropdown.propTypes.position, // eslint-disable-line react/no-typos
-}
+  static defaultProps = {
+    text: null,
+    children: null,
+    className: '',
+    position: 'bc',
+  }
 
-Tooltip.defaultProps = {
-  text: null,
-  children: null,
-  className: '',
-  position: 'bc',
+  render() {
+    const { text, children, position, className } = this.props
+
+    return (
+      <Dropdown
+        button={<span>{children}</span>}
+        hoverMode
+        noWrapperStyle
+        position={position}
+      >
+        <div
+          className={`
+            ${styles.tooltip}
+            ${styles[`position-${position}`]}
+            ${className}
+          `}
+          ref={e => {
+            this.wrapperElement = e
+          }}
+        >
+          {text}
+        </div>
+      </Dropdown>
+    )
+  }
 }
 
 export default Tooltip
