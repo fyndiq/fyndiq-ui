72fb77ab57f057deb8c49d58b3f95f5c9e4781e6
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-11-10 15:54:40 +0100

    :new: Add Table components

---
+++ packages/fyndiq-component-table/src/row.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from '../table.css'
+
+const Row = ({
+  children,
+  head,
+  interactive,
+  className,
+  noBorder,
+  size,
+  verticalCenter,
+}) => (
+  <div
+    className={`
+      ${styles.row}
+      ${head && styles.rowHead}
+      ${interactive && styles.rowInteractive}
+      ${noBorder && styles.rowNoBorder}
+      ${verticalCenter && styles.rowVerticalCenter}
+      ${styles[`rowSize-${size}`]}
+      ${className}
+    `}
+  >
+    {children}
+  </div>
+)
+
+Row.propTypes = {
+  head: PropTypes.bool,
+  interactive: PropTypes.bool,
+  className: PropTypes.string,
+  noBorder: PropTypes.bool,
+  size: PropTypes.oneOf(['s', 'm', 'l']),
+  children: PropTypes.element,
+  verticalCenter: PropTypes.bool,
+}
+
+Row.defaultProps = {
+  className: '',
+  noBorder: false,
+  children: null,
+  head: false,
+  interactive: false,
+  size: 'm',
+  verticalCenter: true,
+}
+
+export default Row
