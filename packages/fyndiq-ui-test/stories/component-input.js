0e4c9a0b362372b850c145d4bb609e82b7447611
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-11-15 16:18:05 +0100

    :wrench: Use nested stories for Input

--- packages/fyndiq-ui-test/stories/component-input.js
+++ packages/fyndiq-ui-test/stories/component-input.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { storiesOf, action } from '@storybook/react'
 import Input, { Presets, InvisibleInput } from 'fyndiq-component-input'
 
-storiesOf('Input', module)
-  .addWithInfo('default', () => <Input onChange={action('onChange')} />)
-  .addWithInfo('with presets', () => (
-    <Presets
-      presets={['Stockholm', 'Göteborg', 'Luleå', 'Kiruna']}
-      onChange={action('onChange')}
-    />
-  ))
-  .addWithInfo('invisible', () => (
-    <InvisibleInput value="initial value" onChange={action('onChange')} />
-  ))
+storiesOf('Input/Input', module).addWithInfo('default', () => (
+  <Input onChange={action('onChange')} />
+))
+
+storiesOf('Input/Presets', module).addWithInfo('with presets', () => (
+  <Presets
+    presets={['Stockholm', 'Göteborg', 'Luleå', 'Kiruna', 'Test city name']}
+    onChange={action('onChange')}
+  />
+))
+
+storiesOf('Input/Invisible', module).addWithInfo('default', () => (
+  <InvisibleInput value="initial value" onChange={action('onChange')} />
+))
