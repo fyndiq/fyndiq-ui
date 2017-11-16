c2074c58032ba4cc05df85d1f2d78c51424c3530
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-11-16 14:24:14 +0100

    :new: Add Bag Icon

--- packages/fyndiq-icons/src/index.js
+++ packages/fyndiq-icons/src/index.js
@@ -1,7 +1,8 @@
 import Arrow from './arrow'
+import Bag from './bag'
 import Star from './star'
 import Checkmark from './checkmark'
 import Magnifier from './magnifier'
 import Pencil from './pencil'
 
-export { Arrow, Star, Checkmark, Magnifier, Pencil }
+export { Arrow, Bag, Star, Checkmark, Magnifier, Pencil }
