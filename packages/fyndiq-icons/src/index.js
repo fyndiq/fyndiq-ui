d0f4df99ddf9c5109549c1e4465b0156d808f592
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-11-22 15:22:05 +0100

    :new: Add Shop Icon

--- packages/fyndiq-icons/src/index.js
+++ packages/fyndiq-icons/src/index.js
@@ -4,8 +4,20 @@ import Checkmark from './checkmark'
 import Error from './error'
 import Magnifier from './magnifier'
 import Pencil from './pencil'
+import Shop from './shop'
 import Star from './star'
 import Truck from './truck'
 import Warning from './warning'
 
-export { Arrow, Bag, Checkmark, Error, Magnifier, Pencil, Star, Truck, Warning }
+export {
+  Arrow,
+  Bag,
+  Checkmark,
+  Error,
+  Magnifier,
+  Pencil,
+  Shop,
+  Star,
+  Truck,
+  Warning,
+}
