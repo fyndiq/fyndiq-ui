f9f884ff47f0052c9700f0a598b3ff6045e41141
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-11-22 10:20:41 +0100

    :new: Message now hides itself after a timeout

--- packages/fyndiq-component-message/src/message.js
+++ packages/fyndiq-component-message/src/message.js
@@ -11,28 +11,81 @@ const colors = {
   error: fyndiqColors.red,
 }
 
-const Message = ({ type, children, icon }) => (
-  <div
-    className={`
-      ${styles.message}
-      ${styles[`message--type_${type}`]}
-    `}
-  >
-    {icon && React.cloneElement(icon, { color: colors[type] })}
-    {children}
-  </div>
-)
+class Message extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.remaining = this.props.timeout
+
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+  }
+
+  componentWillMount() {
+    this.resume()
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout)
+  }
+
+  onMouseEnter() {
+    // Pause the timer
+    this.pause()
+  }
+
+  onMouseLeave() {
+    // Resume the timer
+    this.resume()
+  }
+
+  pause() {
+    clearTimeout(this.timeout)
+    this.remaining -= new Date() - this.start
+
+    // Timer should be at least 1s
+    this.remaining = Math.max(this.remaining, 1000)
+  }
+
+  resume() {
+    this.start = new Date()
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(this.props.onClose, this.remaining)
+  }
+
+  render() {
+    const { type, children, icon } = this.props
+
+    return (
+      <div
+        className={`
+          ${styles.message}
+          ${styles[`message--type_${type}`]}
+        `}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        {icon && React.cloneElement(icon, { color: colors[type] })}
+        {children}
+      </div>
+    )
+  }
+}
 
 Message.propTypes = {
   type: PropTypes.oneOf(['info', 'confirm', 'warn', 'error']),
   children: PropTypes.node,
   icon: PropTypes.element,
+  onClose: PropTypes.func,
+  timeout: PropTypes.number,
 }
 
 Message.defaultProps = {
   type: 'info',
   children: null,
   icon: null,
+  onClose: () => {},
+  timeout: 5000,
 }
 
 export default Message
