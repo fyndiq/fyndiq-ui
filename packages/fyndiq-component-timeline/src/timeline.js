fa8449e44d14542aeb5e22f1b059a13a71402c51
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-12-01 12:31:08 +0100

    :new: Add Timeline Component

---
+++ packages/fyndiq-component-timeline/src/timeline.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from '../timeline.css'
+
+const Timeline = ({ children, type }) => (
+  <div
+    className={`
+      ${styles.timeline}
+      ${styles[`timeline_type_${type}`]}
+    `}
+  >
+    {children}
+  </div>
+)
+
+Timeline.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element),
+  type: PropTypes.oneOf(['white', 'black']),
+}
+
+Timeline.defaultProps = {
+  children: null,
+  type: 'white',
+}
+
+export default Timeline
