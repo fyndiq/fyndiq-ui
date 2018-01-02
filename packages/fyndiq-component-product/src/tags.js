dc2ef596e15e2df58041caedd18c1a08bcd6185a
Thibaut Remy <thibaut.remy@fyndiq.com>
2018-01-02 14:07:15 +0100

    :wrench: Check tags length

--- packages/fyndiq-component-product/src/tags.js
+++ packages/fyndiq-component-product/src/tags.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import styles from '../product.css'
 
 const ProductTags = ({ tags }) =>
-  tags && (
+  tags.length && (
     <div className={styles.tagsWrapper}>
       {tags.map(tag => <span className={styles.tag}>{tag}</span>)}
     </div>
