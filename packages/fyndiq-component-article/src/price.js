337a3c49efcaf0e26ce65c5d5672c4a7a756810d
Thibaut Remy <thibaut.remy@fyndiq.com>
2018-01-26 09:53:04 +0100

    :new: Add fixedHeight prop

--- packages/fyndiq-component-article/src/price.js
+++ packages/fyndiq-component-article/src/price.js
@@ -2,16 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from '../article-price.css'
 
-const Price = ({ children, oldPrice, emphasize }) =>
-  children && (
+const Price = ({ children, oldPrice, emphasize, fixedHeight }) =>
+  (children || fixedHeight) && (
     <span
       className={`
         ${styles.price}
         ${emphasize && styles.emphasize}
+        ${fixedHeight && styles.fixedHeight}
       `}
     >
-      <span className={styles.currentPrice}>{children}</span>
-      <span className={styles.oldPrice}>{oldPrice}</span>
+      <div className={styles.currentPrice}>{children} </div>
+      <div className={styles.oldPrice}>{oldPrice} </div>
     </span>
   )
 
@@ -19,12 +20,14 @@ Price.propTypes = {
   children: PropTypes.string,
   oldPrice: PropTypes.string,
   emphasize: PropTypes.bool,
+  fixedHeight: PropTypes.bool,
 }
 
 Price.defaultProps = {
   children: null,
   oldPrice: null,
   emphasize: true,
+  fixedHeight: false,
 }
 
 export default Price
