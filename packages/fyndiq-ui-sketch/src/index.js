0bc1c1a83014cf03c225e33b0a5c1a62ce5c7250
Thibaut Remy <thibaut.remy@fyndiq.com>
2018-02-13 09:16:22 +0100

    :art: Simplify API surface of sketch package

--- packages/fyndiq-ui-sketch/src/index.js
+++ packages/fyndiq-ui-sketch/src/index.js
@@ -1,5 +1,4 @@
 import symbols from './symbols'
-import SymbolComponent from './symbol-component'
 import Symbols from './symbols-component'
 
-export { symbols, SymbolComponent, Symbols }
+export { symbols, Symbols }
